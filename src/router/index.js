db85947e460b1c5b108fdda883ccbe98fafb03fb
河上元気 <kawakami@core-tech.jp>
2019-05-28 18:32:34 +0900

    Header Top Info add

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
+import Info from '@/components/Info'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/top',
+      name: 'Top',
+      component: Top
+    },
+    {
+      path: '/info',
+      name: 'Info',
+      component: Info
+    },
   ]
 })
