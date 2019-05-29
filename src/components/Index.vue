1c6f272fc97ec2d3527da4c9b508f267430c610f
河上元気 <kawakami@core-tech.jp>
2019-05-29 17:22:45 +0900

    link test

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -4,19 +4,13 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
+        <a href="/top">
+          TOP
         </a>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
+        <a href="/info">
+          INFO
         </a>
       </li>
       <li>
