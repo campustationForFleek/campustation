24807b18650c36456ec0c4aed5c2573610b6be32
河上元気 <kawakami@core-tech.jp>
2020-01-17 13:39:47 +0900

    戻し

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Index from '@/components/index'
-import Top from '@/components/top'
-import Info from '@/components/info'
+import Index from '@/components/Index'
+import Top from '@/components/Top'
+import Info from '@/components/Info'
 
 Vue.use(Router)
 
