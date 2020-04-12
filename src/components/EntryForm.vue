dd3a2004408e8966cabce8d29bbbb6aaed40349b
菅野由佳 <yuuuun.0jam@gmail.com>
2020-04-12 18:51:41 +0900

    お問い合わせに変更

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="entry">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>お問い合わせ</h2>
     <ul>
       <li>
         <router-link to="/">Go to index</router-link>
